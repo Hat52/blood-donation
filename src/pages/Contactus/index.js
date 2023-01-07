1116dd7ccdd90b014fda69de5cdfb0126774eb4a
hamza ali <tanolihamzaali@gmail.com>
2023-01-08 00:10:32 +0500

    add: Contact us page

--- src/pages/Contactus/index.js
+++ src/pages/Contactus/index.js
@@ -1,8 +1,20 @@
+import { Button, Footer, Textfield } from '../../components';
 import './index.css';
 export default function ContactUs() {
 	return (
-		<div>
-			<div className="bg-image"></div>
-		</div>
+		<>
+			<div className="contact-us-container">
+				<div className="bg-image"></div>
+				<div className="field-container">
+					<h1>Get In Touch</h1>
+					<Textfield placeholder="Full Name" />
+					<Textfield placeholder="Phone Number" />
+					<Textfield placeholder="Your Email" />
+					<textarea className="text-area" placeholder="Your Message" />
+					<Button text="Let's go" onClick={() => console.log('')} />
+				</div>
+				<Footer />
+			</div>
+		</>
 	);
 }
